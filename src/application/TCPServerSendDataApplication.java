ed1e6eb62ab6be92bb9b8181cd234bc47e05f2a3
Administrator <Administrator@XDGOSFS6BN7GUBB>
2021-03-23 16:42:35 +0800

    --- automatic rebase to remote; old local SHA: f84fc172e097d7fc20926d46d7b5c31564302f07 ---

--- src/application/TCPServerSendDataApplication.java
+++ src/application/TCPServerSendDataApplication.java
@@ -693,7 +693,7 @@ public HandGuidingMotion createhandGuidingMotion(){
 		motion.setJointVelocityLimit(0.8)
 		.setCartVelocityLimit(900.0).setJointLimitViolationFreezesAll(false)
 		.setJointLimitsMax(Math.toRadians(10), Math.toRadians(0), Math.toRadians(45), Math.toRadians(115), Math.toRadians(160),Math.toRadians(-60), Math.toRadians(165))
-		.setJointLimitsMin(Math.toRadians(-10), Math.toRadians(-50), Math.toRadians(-45), Math.toRadians(0), Math.toRadians(-160),Math.toRadians(-110), Math.toRadians(-165))
+		.setJointLimitsMin(Math.toRadians(-10), Math.toRadians(-43), Math.toRadians(-45), Math.toRadians(0), Math.toRadians(-160),Math.toRadians(-110), Math.toRadians(-165))
 		.setJointLimitsEnabled(true,true,true,true,true,true,true)
 
 
