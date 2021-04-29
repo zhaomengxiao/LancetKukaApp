6a3fd6bf01c499e8070bf282ceeb5323abc4b650
Chen Peng <18868874900@163.com>
2021-04-29 22:41:56 +0800

    --- automatic rebase to remote; old local SHA: a1acea51594f3bacf286181585cedb48555cabce ---

--- src/application/WallTest.java
+++ src/application/WallTest.java
@@ -605,13 +605,18 @@ public class WallTest extends RoboticsAPIApplication {
 						if(dfa<-0.3){
 							dfa=-0.3;
 						}
-						
+						if(dfb>0.3){
+							dfb=0.3;
+						}
+						if(dfb<-0.3){
+							dfb=-0.3;
+						}
 //						Frame Ptest2 = getApplicationData().getFrame("/CoverScrewing/SmallCover").copyWithRedundancy().transform((Transformation.ofTranslation(0, 200, 0)));
 //						Frame Ptest2 = destFrame.transform((Transformation.ofTranslation(dfx, dfy, 0)));
 						Frame Ptest3 = destFrame_Dangerous.transform((Transformation.ofTranslation(dfx*0.1, dfy*0.1, 0)));
 
 //						Frame Ptest2 = destFrame.transform((Transformation.ofRad(dfx, dfy, dfz, dfa, dfb, dfc)));
-						Frame Ptest2 = destFrame.transform((Transformation.ofRad(dfx*2, 0, dfz*2, 0, 0, 0)));
+						Frame Ptest2 = destFrame.transform((Transformation.ofRad(dfx*2, 0, dfz*2, 0, dfb, 0)));
 //						Frame Ptest2 = destFrame.transform((Transformation.ofRad(0, 0, 0, 0, 0, 0)));
 //						destFrame.setX(destFrame.getX() + dfx);
 //						destFrame.setY(destFrame.getY() + dfy);
