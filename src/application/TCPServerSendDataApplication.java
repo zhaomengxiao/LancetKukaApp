0ba98faf6d48e032b592e707788fff8efc7c78a6
Administrator <Administrator@YP2G3WIXEM5G20F>
2021-07-21 20:44:15 +0800

    --- automatic rebase to remote; old local SHA: b6181f1afac11f32cae79364258be7f0d5c1a2a5 ---

--- src/application/TCPServerSendDataApplication.java
+++ src/application/TCPServerSendDataApplication.java
@@ -1241,8 +1241,8 @@ public  class motion implements Callable<String> {
 	    protected CartesianImpedanceControlMode ZoneLimit()
 	    {
 	        final CartesianImpedanceControlMode cartImp = new CartesianImpedanceControlMode();
-	        cartImp.parametrize(CartDOF.X).setStiffness(4000.0);
-	        cartImp.parametrize(CartDOF.Y).setStiffness(4000.0);
+	        cartImp.parametrize(CartDOF.X).setStiffness(2000.0);
+	        cartImp.parametrize(CartDOF.Y).setStiffness(2000.0);
 	        cartImp.parametrize(CartDOF.Z).setStiffness(2000.0);
 	        cartImp.parametrize(CartDOF.ROT).setStiffness(300.0);
 
@@ -1488,7 +1488,7 @@ public  class motion implements Callable<String> {
 		final CartesianImpedanceControlMode cartImp22 = ZoneLimit();
 
 //		lbr.move(lin(getFrame("/p2")).setCartVelocity(800).setMode(cartImp));
-		 lbr.move(new PTP(new JointPosition( 0.5, 0.8, 0.2, 1.0, -0.5, -0.5, -1.5)).setMode(cartImp22));
+		 lbr.move(new PTP(new JointPosition( 0.5, 0.8, 0.2, 1.0, -0.5, -0.5, -1.2)).setMode(cartImp22));
 		 System.out.println("finish");
 		 nWorkingmode=0;
 		
