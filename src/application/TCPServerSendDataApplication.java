386b8da5330acb4acf94ee01bc90ae91c6bb888e
Administrator <Administrator@YP2G3WIXEM5G20F>
2021-07-22 14:08:18 +0800

    --- automatic rebase to remote; old local SHA: 88c52ae161fc7625b097fd150da0d2ad8e63b1c8 ---

--- src/application/TCPServerSendDataApplication.java
+++ src/application/TCPServerSendDataApplication.java
@@ -3140,36 +3140,36 @@ public  class motion implements Callable<String> {
 	public void run()  {
 		JointPosition actPos = lbr.getCurrentJointPosition();
 		
-//		BreakTest.initialize();
-//		BreakTest.run();
+		BreakTest.initialize();
+		BreakTest.run();
 
-		double offset=10;
-		Frame toolcurFrame=lbr.getCurrentCartesianPosition(needle.getDefaultMotionFrame());
-		Frame target = toolcurFrame.copyWithRedundancy();
-		target.setX(target.getX()+offset);
-		
-		LIN linMotion = new LIN(target);
-		needle.getDefaultMotionFrame().move(linMotion);
+//		double offset=10;
+//		Frame toolcurFrame=lbr.getCurrentCartesianPosition(needle.getDefaultMotionFrame());
+//		Frame target = toolcurFrame.copyWithRedundancy();
+//		target.setX(target.getX()+offset);
+//		
+//		LIN linMotion = new LIN(target);
+//		needle.getDefaultMotionFrame().move(linMotion);
 //		ISafetyState currentState = lbr.getSafetyState();
 	
 		//jjj
-//		ExecutorService executor = Executors.newCachedThreadPool();
-//		Future<String> add = executor.submit(new sendRTdata());
-//		Future<String> say = executor.submit(new motion());
-//		Future<String> sdd2 = executor.submit(new reciveRTdata());
-//        //Monitor();
-//
-//		try {
-//			System.out.println(add.get());
-//			System.out.println(say.get());
-//			System.out.println(sdd2.get());
-//		} catch (InterruptedException e) {
-//			// TODO è‡ªåŠ¨ç”Ÿæˆ�çš„ catch å�—
-//			e.printStackTrace();
-//		} catch (ExecutionException e) {
-//			// TODO è‡ªåŠ¨ç”Ÿæˆ�çš„ catch å�—
-//			e.printStackTrace();
-//		} 
+		ExecutorService executor = Executors.newCachedThreadPool();
+		Future<String> add = executor.submit(new sendRTdata());
+		Future<String> say = executor.submit(new motion());
+		Future<String> sdd2 = executor.submit(new reciveRTdata());
+        //Monitor();
+
+		try {
+			System.out.println(add.get());
+			System.out.println(say.get());
+			System.out.println(sdd2.get());
+		} catch (InterruptedException e) {
+			// TODO è‡ªåŠ¨ç”Ÿæˆ�çš„ catch å�—
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO è‡ªåŠ¨ç”Ÿæˆ�çš„ catch å�—
+			e.printStackTrace();
+		} 
 
 		
 		
