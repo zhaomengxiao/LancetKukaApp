d6c27049395aa3337abb4fd262c5f646b297372a
Chen Peng <18868874900@163.com>
2021-07-28 20:20:51 +0800

    --- automatic rebase to remote; old local SHA: af3afb49705f8d28afee6cf54899e750a308230d ---

--- src/application/motion_Y_Negtive.java
+++ src/application/motion_Y_Negtive.java
@@ -223,7 +223,7 @@ public class motion_Y_Negtive extends RoboticsAPIApplication
             double nAix4=0;
             //for (i = 0; i < NUM_RUNS; ++i)
             boolean bOnlyForPlane=false;
-    	 while(Math.abs(nAix4)<115 && bOnlyForPlane==false && nDistance<105)
+    	 while(Math.abs(nAix4)<115 && bOnlyForPlane==false && nDistance>-105)
             {
 //    		 System.out.println(i1);
     		 bOnlyForPlane=_vi.MotionType();
