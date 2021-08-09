a066db61014a2a1cfc3fa9fbc7437f58f3b24772
Chen Peng <18868874900@163.com>
2021-08-09 16:15:34 +0800

    --- automatic rebase to remote; old local SHA: 8e7df4c87133b01cadc4acaca778f412a657fea4 ---

--- src/application/TCPServerSendDataApplication.java
+++ src/application/TCPServerSendDataApplication.java
@@ -1272,8 +1272,8 @@ public HandGuidingMotion createhandGuidingMotion(){
 		        final CartesianImpedanceControlMode cartImp = new CartesianImpedanceControlMode();
 		
 		        cartImp.parametrize(CartDOF.X).setStiffness(5000.0);
-		        cartImp.parametrize(CartDOF.Y).setStiffness(2000.0);
-		        cartImp.parametrize(CartDOF.Z).setStiffness(2000.0);
+		        cartImp.parametrize(CartDOF.Y).setStiffness(50.0);
+		        cartImp.parametrize(CartDOF.Z).setStiffness(50.0);
 		        cartImp.parametrize(CartDOF.ROT).setStiffness(300.0);
 
 //		        System.out.println(nStiff);
