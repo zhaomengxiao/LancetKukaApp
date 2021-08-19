981c6d3a35c914de48582bbfe5932edb600d3082
Administrator <Administrator@YP2G3WIXEM5G20F>
2021-08-19 10:10:25 +0800

    --- automatic rebase to remote; old local SHA: c9417efdd6ce2f019452604d00632a958288eaa9 ---

--- src/application/CopyOfTeachingByHand_2.java
+++ src/application/CopyOfTeachingByHand_2.java
@@ -30,25 +30,7 @@ import com.kuka.roboticsAPI.motionModel.controlModeModel.PositionControlMode;
 //import com.kuka.statuscontroller.api.IStatusController;
 
 /**
- * This example activates a SmartServo motion in Cartesian impedance control mode, sends a sequence of C55555555555555522222222222222222222222222222222222222222222222500000000000050000050500000000000000000000000000050000505050505050505050505050500000000000000000050
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * 
- * artesian set
+ * This example activates a SmartServo motion in Cartesian impedance control mode, sends a sequence of Cartesian set
  * points and modifies compliance parameters during the motion.
  * 
  */
