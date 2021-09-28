07cbd640bf44eeda9a1eacaf488a506e1fa6d1eb
Administrator <Administrator@YP2G3WIXEM5G20F>
2021-09-28 19:40:23 +0800

    --- automatic rebase to remote; old local SHA: f755144f3762f841478e22950127f4a7651a7a2c ---

--- src/application/TCPServerSendDataApplication.java
+++ src/application/TCPServerSendDataApplication.java
@@ -1367,13 +1367,13 @@ public HandGuidingMotion createhandGuidingMotion(){
 		        return cartImp; 	
 		    }
 		    
-		    protected CartesianImpedanceControlMode HardLimit_Y()
+		    protected CartesianImpedanceControlMode HardLimit_X()
 		    {
 
 		        final CartesianImpedanceControlMode cartImp = new CartesianImpedanceControlMode();
 		
-		        cartImp.parametrize(CartDOF.X).setStiffness(5000.0);
-		        cartImp.parametrize(CartDOF.Y).setStiffness(4500.0);
+		        cartImp.parametrize(CartDOF.Y).setStiffness(5000.0);
+		        cartImp.parametrize(CartDOF.X).setStiffness(4500.0);
 		        cartImp.parametrize(CartDOF.Z).setStiffness(5000.0);
 		        cartImp.parametrize(CartDOF.ROT).setStiffness(300.0);
 
@@ -2441,7 +2441,7 @@ public HandGuidingMotion createhandGuidingMotion(){
 					
 			    					
 			    	final CartesianImpedanceControlMode carthard = HardLimit();
-			    	final CartesianImpedanceControlMode carthard_Y = HardLimit_Y();
+			    	final CartesianImpedanceControlMode carthard_X = HardLimit_X();
 			    	final CartesianImpedanceControlMode carthard_Z = HardLimit_Z();
 			    	if(nToolMode==3){
 				    	Frame cmdPos2 = lbr.getCurrentCartesianPosition(needle.getFrame("/tcp_x_1_yz3"));
@@ -2451,15 +2451,15 @@ public HandGuidingMotion createhandGuidingMotion(){
 		       	        Transformation DistanceToPlane=Ptest_ForPlane.staticTransformationTo(cmdPos2);
 	        	   
 		       	        count++;
-		        	    nintegral=nintegral+DistanceToPlane.getX();
-		         	    nderivative=DistanceToPlane.getX()-nPrevious_error;
-		        	    nOutput=nP*DistanceToPlane.getX()+nI*nintegral+nD*nderivative;
-		        	    nPrevious_error=DistanceToPlane.getX();
+		        	    nintegral=nintegral+DistanceToPlane.getY();
+		         	    nderivative=DistanceToPlane.getY()-nPrevious_error;
+		        	    nOutput=nP*DistanceToPlane.getY()+nI*nintegral+nD*nderivative;
+		        	    nPrevious_error=DistanceToPlane.getY();
 		            	Ptest_ForPlane1 = Ptest_ForPlane.copyWithRedundancy().transform((Transformation.ofTranslation(-nOutput, 0, 0))); 
 		        	   
-					    if(Math.abs(DistanceToPlane.getY())>130){
+					    if(Math.abs(DistanceToPlane.getX())>130){
 //					    	if(count%100==0){
-					    		System.out.println("DistanceToPlane.getY())>100");
+					    		System.out.println("DistanceToPlane.getX())>100");
 //					    	}
 					    	
 					    		cmdPos2 = lbr.getCurrentCartesianPosition(needle.getFrame("/tcp_x_1_yz3"));
@@ -2537,13 +2537,13 @@ public HandGuidingMotion createhandGuidingMotion(){
 		       	        Transformation DistanceToPlane=Ptest_ForPlane.staticTransformationTo(cmdPos2);
 	        	   
 		       	        count++;
-		        	    nintegral=nintegral+DistanceToPlane.getX();
-		         	    nderivative=DistanceToPlane.getX()-nPrevious_error;
-		        	    nOutput=nP*DistanceToPlane.getX()+nI*nintegral+nD*nderivative;
-		        	    nPrevious_error=DistanceToPlane.getX();
+		        	    nintegral=nintegral+DistanceToPlane.getY();
+		         	    nderivative=DistanceToPlane.getY()-nPrevious_error;
+		        	    nOutput=nP*DistanceToPlane.getY()+nI*nintegral+nD*nderivative;
+		        	    nPrevious_error=DistanceToPlane.getY();
 		            	Ptest_ForPlane1 = Ptest_ForPlane.copyWithRedundancy().transform((Transformation.ofTranslation(-nOutput, 0, 0))); 
 		        	   
-					    if(Math.abs(DistanceToPlane.getY())> 120){
+					    if(Math.abs(DistanceToPlane.getX())> 120){
 //					    	if(count%100==0){
 					    		System.out.println("DistanceToPlane.getY())>100");
 //					    	}
@@ -2594,7 +2594,7 @@ public HandGuidingMotion createhandGuidingMotion(){
 		            		count=0;
 		            	}
 		            	if(count%30==0){
-		            		System.out.println("DistanceToPlane_x："+DistanceToPlane.getX()+"DistanceToPlane_c："+Math.toDegrees(DistanceToPlane.getGammaRad())); 
+		            		System.out.println("DistanceToPlane_Y："+DistanceToPlane.getY()+"DistanceToPlane_c："+Math.toDegrees(DistanceToPlane.getGammaRad())); 
 		                  //System.out.println("DistanceToPlane_x："+DistanceToPlane.getX()+"DistanceToPlane_Y："+DistanceToPlane.getY()+"DistanceToPlane_Z："+DistanceToPlane.getZ());
 		            	}
 			    	}
