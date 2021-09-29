16e2026c51754b7e39fb5db6b2bdf07f8dd963a3
Administrator <Administrator@YP2G3WIXEM5G20F>
2021-09-29 16:19:16 +0800

    --- automatic rebase to remote; old local SHA: 2f34db117c4008cd47a99c84171748d311c6e222 ---

--- src/application/TCPServerSendDataApplication.java
+++ src/application/TCPServerSendDataApplication.java
@@ -2714,13 +2714,13 @@ public HandGuidingMotion createhandGuidingMotion(){
 		       	        Transformation DistanceToPlane=Ptest_ForPlane.staticTransformationTo(cmdPos2);
 	        	   
 		       	        count++;
-		        	    nintegral=nintegral+DistanceToPlane.getY();
-		         	    nderivative=DistanceToPlane.getY()-nPrevious_error;
-		        	    nOutput=nP*DistanceToPlane.getY()+nI*nintegral+nD*nderivative;
-		        	    nPrevious_error=DistanceToPlane.getY();
+		        	    nintegral=nintegral+DistanceToPlane.getX();
+		         	    nderivative=DistanceToPlane.getX()-nPrevious_error;
+		        	    nOutput=nP*DistanceToPlane.getX()+nI*nintegral+nD*nderivative;
+		        	    nPrevious_error=DistanceToPlane.getX();
 		            	Ptest_ForPlane1 = Ptest_ForPlane.copyWithRedundancy().transform((Transformation.ofTranslation(-nOutput, 0, 0))); 
 		        	   
-					    if(Math.abs(DistanceToPlane.getX())> 120){
+					    if(Math.abs(DistanceToPlane.getY())> 120){
 //					    	if(count%100==0){
 					    		System.out.println("DistanceToPlane.getY())>100");
 //					    	}
@@ -2741,7 +2741,7 @@ public HandGuidingMotion createhandGuidingMotion(){
 					    		nWorkingmode=0;
 					    }
 					   
-					    else if(Math.abs(DistanceToPlane.getZ())>200){
+					    else if(Math.abs(DistanceToPlane.getZ())>120){
 //					    	if(count%100==0){
 					    		System.out.println("DgetZ()");
 					    		Ptest_ForPlane1.setX(cmdPos2.getX());
@@ -2771,7 +2771,7 @@ public HandGuidingMotion createhandGuidingMotion(){
 		            		count=0;
 		            	}
 		            	if(count%30==0){
-		            		System.out.println("DistanceToPlane_Y："+DistanceToPlane.getY()+"DistanceToPlane_c："+Math.toDegrees(DistanceToPlane.getGammaRad())); 
+		            		System.out.println("DistanceToPlane_X："+DistanceToPlane.getX()+"DistanceToPlane_c："+Math.toDegrees(DistanceToPlane.getGammaRad())); 
 		                  //System.out.println("DistanceToPlane_x："+DistanceToPlane.getX()+"DistanceToPlane_Y："+DistanceToPlane.getY()+"DistanceToPlane_Z："+DistanceToPlane.getZ());
 		            	}
 			    	}
@@ -2784,13 +2784,13 @@ public HandGuidingMotion createhandGuidingMotion(){
 		       	        Transformation DistanceToPlane=Ptest_ForPlane.staticTransformationTo(cmdPos2);
 	        	   
 		       	        count++;
-		        	    nintegral=nintegral+DistanceToPlane.getY();
-		         	    nderivative=DistanceToPlane.getY()-nPrevious_error;
-		        	    nOutput=nP*DistanceToPlane.getY()+nI*nintegral+nD*nderivative;
-		        	    nPrevious_error=DistanceToPlane.getY();
+		        	    nintegral=nintegral+DistanceToPlane.getX();
+		         	    nderivative=DistanceToPlane.getX()-nPrevious_error;
+		        	    nOutput=nP*DistanceToPlane.getX()+nI*nintegral+nD*nderivative;
+		        	    nPrevious_error=DistanceToPlane.getX();
 		            	Ptest_ForPlane1 = Ptest_ForPlane.copyWithRedundancy().transform((Transformation.ofTranslation(-nOutput, 0, 0))); 
 		        	   
-					    if(Math.abs(DistanceToPlane.getX())> 120){
+					    if(Math.abs(DistanceToPlane.getY())> 120){
 //					    	if(count%100==0){
 					    		System.out.println("DistanceToPlane.getY())>100");
 //					    	}
@@ -2841,7 +2841,7 @@ public HandGuidingMotion createhandGuidingMotion(){
 		            		count=0;
 		            	}
 		            	if(count%30==0){
-		            		System.out.println("DistanceToPlane_Y："+DistanceToPlane.getY()+"DistanceToPlane_c："+Math.toDegrees(DistanceToPlane.getGammaRad())); 
+		            		System.out.println("DistanceToPlane_X："+DistanceToPlane.getX()+"DistanceToPlane_c："+Math.toDegrees(DistanceToPlane.getGammaRad())); 
 		                  //System.out.println("DistanceToPlane_x："+DistanceToPlane.getX()+"DistanceToPlane_Y："+DistanceToPlane.getY()+"DistanceToPlane_Z："+DistanceToPlane.getZ());
 		            	}
 			    	}
