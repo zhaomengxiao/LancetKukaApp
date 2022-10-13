403b41d3cffca312e3df7a8f62e7bbf46aef11ee
Zhao Youjun <597153372@qq.com>
2022-10-13 15:00:09 +0800

    --- automatic rebase to remote; old local SHA: 54caa2c93317a0749f8830ee7071e90b433aa4bf ---

--- examples/com/kuka/connectivity/fri/example/TransformationProvider.java
+++ examples/com/kuka/connectivity/fri/example/TransformationProvider.java
@@ -1,5 +1,7 @@
 package com.kuka.connectivity.fri.example;
 
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.ptp;
+
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -10,8 +12,12 @@ import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
 import com.kuka.roboticsAPI.controllerModel.Controller;
 import com.kuka.roboticsAPI.deviceModel.LBR;
 import com.kuka.roboticsAPI.geometricModel.Frame;
+import com.kuka.roboticsAPI.geometricModel.LoadData;
+import com.kuka.roboticsAPI.geometricModel.ObjectFrame;
+import com.kuka.roboticsAPI.geometricModel.Tool;
 import com.kuka.roboticsAPI.geometricModel.World;
 import com.kuka.roboticsAPI.geometricModel.math.Transformation;
+import com.kuka.roboticsAPI.geometricModel.math.XyzAbcTransformation;
 
 /**
  * Creates a FRI Session.
@@ -22,6 +28,18 @@ public class TransformationProvider extends RoboticsAPIApplication
     private LBR _lbr;
     private String _clientName;
     private boolean on  = true;
+    
+    
+    // Tool Data
+    private Tool _toolAttachedToLBR;
+    private LoadData _loadData;
+    private static final String TOOL_FRAME = "toolFrame";
+    private static final double[] TRANSLATION_OF_TOOL = { 200, 200, 200 };
+    private static final double MASS = 0;
+    private static final double[] CENTER_OF_MASS_IN_MILLIMETER = { 200, 200, 200 };
+
+   
+    
     @Override
     public void initialize()
     {
@@ -31,6 +49,24 @@ public class TransformationProvider extends RoboticsAPIApplication
         // *** change next line to the FRIClient's IP address                 ***
         // **********************************************************************
         _clientName = "172.31.1.148";
+        
+        // Create a Tool by Hand this is the tool we want to move with some mass
+        // properties and a TCP-Z-offset of 100.
+        _loadData = new LoadData();
+        _loadData.setMass(MASS);
+        _loadData.setCenterOfMass(
+                CENTER_OF_MASS_IN_MILLIMETER[0], CENTER_OF_MASS_IN_MILLIMETER[1],
+                CENTER_OF_MASS_IN_MILLIMETER[2]);
+        _toolAttachedToLBR = new Tool("Tool", _loadData);
+
+        XyzAbcTransformation trans = XyzAbcTransformation.ofTranslation(
+                TRANSLATION_OF_TOOL[0], TRANSLATION_OF_TOOL[1],
+                TRANSLATION_OF_TOOL[2]);
+        ObjectFrame aTransformation = _toolAttachedToLBR.addChildFrame(TOOL_FRAME
+                + "(TCP)", trans);
+        _toolAttachedToLBR.setDefaultMotionFrame(aTransformation);
+        // Attach tool to the robot
+        _toolAttachedToLBR.attachTo(_lbr.getFlange());
 
     }
 
@@ -73,14 +109,29 @@ public class TransformationProvider extends RoboticsAPIApplication
         }
 
         getLogger().info("FRI connection established.");
+     // move to start pose
+        _lbr.move(ptp(.0, .0, .0, Math.toRadians(90), .0, .0, .0));
 
         // Output
         getLogger().info("Transformation from World of");
         while (on)
         {
-            ThreadUtil.milliSleep(150);
-            getLogger().info("Frame objectBase:\n" + objectBase.toStringInWorld());
-            getLogger().info("Frame objectTip:\n" + objectTip.toStringInWorld());
+            //ThreadUtil.milliSleep(150);
+        	try{
+        		_toolAttachedToLBR.move(ptp(objectBase).setJointVelocityRel(0.2));
+        	}
+        	catch (IllegalStateException e){
+        		getLogger().error(e.getLocalizedMessage());
+        	}
+        	
+            // async move with overlay ...
+//            _lbr.moveAsync(ptp(Math.toRadians(-90), .0, .0, Math.toRadians(90), .0, Math.toRadians(-90), .0)
+//                    .setJointVelocityRel(0.2)
+//                    .addMotionOverlay(jointOverlay)
+//                    .setBlendingRel(0.1)
+//                    );
+//            getLogger().info("Frame objectBase:\n" + objectBase.toStringInWorld());
+//            getLogger().info("Frame objectTip:\n" + objectTip.toStringInWorld());
         }
 
         // done
